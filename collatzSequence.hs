367795a140c8f8a75bbb2b6b715f04026b7404ca
Simon Chaffetz <simis1000@gmail.com>
2014-09-17 13:05:19 -0700

    Find all long collatz sequences in 1..100

--- collatzSequence.hs
+++ collatzSequence.hs
@@ -9,7 +9,10 @@ chain n
   | even n = n:chain (n `div` 2)
   | odd n = n:chain(n * 3 + 1)
 
--- for all numbers between 1 and 100, how many chains
--- are there that start with those numbers and are longer
--- than 15 steps long?
+-- for every number n between 1 and 100, how many chains
+-- are there that start with n and are longer
+-- than 15 numbers lon?
+
+longerThan15collatzSequences :: Int
+longerThan15collatzSequences = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 
